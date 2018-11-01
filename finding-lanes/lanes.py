6e52cfe7991d0e317215b9cfd8c3b993fd97d018
Mahbub - A - Rob <mahbub.rob3@gmail.com>
2018-11-02 01:16:40 +0600

    apply blur, gaussian filter, canny function

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -12,18 +12,30 @@ image = cv2.imread("test_image.jpg")
 # Copy Image to a numpy array
 lane_image = np.copy(image)
 
-# Step 1: 
-# Create a grayscale image
-# Why? We will detect lane lines using black and white color values
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 
+# Canny function
+def canny(image):
+    # Step 1: 
+    # Create a grayscale image
+    # Why? 
+    # 1. gray scale image has single channel where 
+    # colored image has more at least 3 channels, 
+    # So, gray scale computation is fast and less computing power needed. 
+    # And, we will detect lane lines using black and white color values
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    
+    
+    # Step 2: Reduce noise - Apply Guasian Filter
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) 
+    
+    # Find the strongest gradients of the image
+    canny = cv2.Canny(blur, 50, 150)
+    return canny    
+    
+ 
+canny = canny(lane_image)   
 
-# Step 2: Reduce noise - Apply Guasian Filter
-blur = cv2.GaussianBlur(gray, (5, 5), 0) 
-
-# Use gradient of the image
-canny = cv2.Canny(blur, 50, 150)
-
+ 
 # Show Image
 cv2.namedWindow('result', cv2.WINDOW_AUTOSIZE)
 cv2.imshow("result", canny)
